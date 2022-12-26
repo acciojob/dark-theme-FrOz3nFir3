113cf5f131a0778b4ca67a002f9d69affdcd7ad9
Kush Surti <kushsurti05@gmail.com>
2022-12-26 15:32:19 +0530

    added switching of themes

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-//your code here
+var divApp = document.querySelector("#app");
+var buttonSwitch = document.querySelector("#swap");
 
+buttonSwitch.addEventListener("click", changeTheme);
+
+function changeTheme() {
+  buttonSwitch.classList.toggle("button_night");
+  divApp.classList.toggle("night");
+}
